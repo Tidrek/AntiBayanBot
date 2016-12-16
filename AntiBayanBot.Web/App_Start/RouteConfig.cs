90bce6a5a5e4e485f1c75b951c6d6073c14f53cd
aavanesov <avanesov.andrey@gmail.com>
2016-12-17 03:16:09 +0500

    Solution infrastructure.

---
+++ AntiBayanBot.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AntiBayanBot.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Default", "{controller}/{action}", new { controller = "Home", action = "Index" });
+        }
+    }
+}
---
+++ AntiBayanBot.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AntiBayanBot.Web.MvcApplication" Language="C#" %>
