f48ddc0904cc6261a83163b7a99d5819d14568ad
Orif <freethinking@inbox.ru>
2016-12-17 23:05:52 +0500

    A bit of refactoring

--- AntiBayanBot.Web/Controllers/AntiBayanBotController.cs
+++ AntiBayanBot.Web/Controllers/AntiBayanBotController.cs
@@ -1,25 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.IO;
-using File = System.IO.File;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using System.Drawing;
-using System.Security.Cryptography;
 
 
 namespace AntiBayanBot.Web.Controllers
 {
     public class AntiBayanBotController : ApiController
     {
-        public readonly TelegramBotClient Bot = new TelegramBotClient(ConfigurationManager.AppSettings["AntiBayanBotToken"]);
+        private readonly TelegramBotClient Bot = new TelegramBotClient(ConfigurationManager.AppSettings["AntiBayanBotToken"]);
 
         public async Task<IHttpActionResult> Post(Update update)
         {
@@ -30,34 +23,11 @@ namespace AntiBayanBot.Web.Controllers
             {
                 // Download Photo
                 var file = await Bot.GetFileAsync(message.Photo.LastOrDefault()?.FileId);
-                string imageExt = file.FilePath.Split('.').Last().ToLower();
+                string imageExt = file.FilePath.Split('.').Last();
 
-                if (imageExt == "gif")
-                {
-                    //Compute md5
-                    string hash = null;
-                    using (var md5 = MD5.Create())
-                    {
-                        byte[] buffer = null;
-                        file.FileStream.Read(buffer, 0, file.FileSize);
-                        hash = BitConverter.ToString(md5.ComputeHash(buffer)).Replace("-", "‌​").ToLower(); //standard looking md5              
-                    }
-                    //Get existing hashes and compare
-                    //Set bayanDetected = true if hash is found 
-                }
-                else { 
-                    //Get image features
-                    var bitmap = new Bitmap(file.FileStream);
-
-                    //Get existing features and compare
-                    //Set bayanDetected = true if chance is high            
-                }
-
-                if(bayanDetected)
-                { 
-                    await Bot.SendTextMessageAsync(message.Chat.Id, text: GetPunishMessage(), replyToMessageId: message.MessageId);
-                }
-                else
+                bayanDetected = new BayanDetector().DetectPhotoBayan(file.FileStream, file.FileSize, imageExt);
+                
+                if(!bayanDetected) //save feature in DB
                 {
                     var obj = new
                     {
@@ -68,6 +38,13 @@ namespace AntiBayanBot.Web.Controllers
                     };
                 }
             }
+
+            //Check text messages...?
+
+            if (bayanDetected)
+            {
+                await Bot.SendTextMessageAsync(message.Chat.Id, text: GetPunishMessage(), replyToMessageId: message.MessageId);
+            }            
 
             return Ok();
         }
